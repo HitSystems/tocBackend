ef3f807f64f5fba7fe78b81ede0cb168e8cab22f
Santiago Álvarez Osuca <82sao82@gmail.com>
2021-11-25 10:05:51 +0100

    Impresora serie{

--- src/impresora/impresora.class.ts
+++ src/impresora/impresora.class.ts
@@ -152,10 +152,10 @@ export class Impresora {
             {
                 if(tipoImpresora === 'SERIE')
                 {
-                    var device = new escpos.Serial('/dev/ttyS0', {
+                    var device = new escpos.Serial('/dev/usb/lp1', {
                         baudRate: 115000,
                         stopBit: 2
-                        });
+                    });
                 }
             }
     
