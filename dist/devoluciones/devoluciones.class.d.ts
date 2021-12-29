6dca000df58b587d6ea964878bbe54538a05d098
Santiago Álvarez Osuca <82sao82@gmail.com>
2021-12-29 17:14:03 +0100

    Mesas

--- dist/devoluciones/devoluciones.class.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Devoluciones {
-    nuevaDevolucion(total: number, idCesta: number): Promise<boolean>;
-    private insertarDevolucion;
-}
-export declare const devolucionesInstance: Devoluciones;
