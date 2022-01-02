3772db90f9ea53386441029b867b74ebc485da74
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-01-02 20:59:10 +0100

    test

--- src/sincro.ts
+++ src/sincro.ts
@@ -16,7 +16,7 @@ function sincronizarTickets() {
                     //     parametros,
                     //     arrayTickets: res
                     // });
-                    console.log(socket.sendBuffer);
+                    // console.log(socket.sendBuffer);
                 }
             }).catch((err) => {
                 console.log(err);
