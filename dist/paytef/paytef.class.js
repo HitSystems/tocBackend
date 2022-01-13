bf070ccf4da1a9094f11265977300e0af4340f7b
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-01-13 10:52:58 +0100

    Paytef para todos

--- dist/paytef/paytef.class.js
+++ dist/paytef/paytef.class.js
@@ -2,9 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.paytefInstance = void 0;
 const axios_1 = require("axios");
+const parametros_clase_1 = require("../parametros/parametros.clase");
+function limpiarNombreTienda(cadena) {
+    const devolver = Number(cadena.replace(/\D/g, ''));
+    if (isNaN(devolver) == false) {
+        return devolver;
+    }
+    else {
+        return 0;
+    }
+}
 class PaytefClass {
     iniciarTransaccion(cantidad, idTicket, idCesta) {
-        return axios_1.default.post('http://10.129.118.29:8887/transaction/start', {
+        const params = parametros_clase_1.parametrosInstance.getParametros();
+        return axios_1.default.post(`http://10.129.${limpiarNombreTienda(params.nombreTienda)}.29:8887/transaction/start`, {
             pinpad: "*",
             opType: "sale",
             cardNumberHashDomain: "branch",
--- src/paytef/paytef.class.ts
+++ src/paytef/paytef.class.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 import { socketInterno } from "../sockets.gateway";
 import { movimientosInstance } from "../movimientos/movimientos.clase";
+import { parametrosInstance } from "src/parametros/parametros.clase";
+
+function limpiarNombreTienda(cadena: string) {
+  const devolver = Number(cadena.replace(/\D/g, ''));
+  if (isNaN(devolver) == false) {
+    return devolver;
+  } else {
+    return 0;
+  }
+}
 
 class PaytefClass {
     iniciarTransaccion(cantidad: number, idTicket: number, idCesta: number) {
-      return axios.post('http://10.129.118.29:8887/transaction/start', {
+      const params = parametrosInstance.getParametros();
+      return axios.post(`http://10.129.${limpiarNombreTienda(params.nombreTienda)}.29:8887/transaction/start`, {
               pinpad: "*",
               opType: "sale",
               cardNumberHashDomain: "branch",
