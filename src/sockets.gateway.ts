5f6cad7a66b23c35e836c941680ac277bd6ae1e4
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-01-21 12:09:40 +0100

    cnf también lo lee

--- src/sockets.gateway.ts
+++ src/sockets.gateway.ts
@@ -95,14 +95,25 @@ export class SocketGateway{
         let tpv: number = null;
         let file = null;
         let arr = [];
-  
+        let errorLecturaArchivo = false;
         try {
           file = fs.readFileSync("/home/hit/clearOne/CoLinux.cfg", "utf8");
           arr = file.split(/\r?\n/);
         } catch(err) {
           console.log("Error: No se ha podido leer el archivo CoLinux");
+          errorLecturaArchivo = true;
         }
         
+        if (errorLecturaArchivo) {
+          try {
+            file = fs.readFileSync("/home/hit/clearOne/CoLinux.cnf", "utf8");
+            arr = file.split(/\r?\n/);
+          } catch(err) {
+            console.log("Error: No se ha podido leer el archivo CoLinux");
+            errorLecturaArchivo = true;
+          }
+        }
+
         for (let i = 0; i < arr.length; i++) {
           if (arr[i].includes('[CLIENTE]')) {
             ventaCliente = Number(arr[i+1]);
