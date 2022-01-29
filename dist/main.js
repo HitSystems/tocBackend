a72ced657585a213954cb5a327fce7af80194a27
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-01-29 01:01:04 +0100

    IPv4 Siempre

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,7 @@ async function bootstrap() {
             origin: true,
             credentials: true
         } });
-    await app.listen(3000);
+    await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
