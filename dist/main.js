607a8b7e03f32f49e509c7ca534702753135b42f
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-02-10 12:22:12 +0100

    Revert "eeeeee"
    This reverts commit e540e339810d4d3546d0bb1ea60d7a2fb459fa8f.

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const axios_1 = require("axios");
 require('./sincro');
-axios_1.default.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://34.78.247.153:3001') : ('http://34.78.247.153:3001');
+axios_1.default.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://63.35.219.5:3001') : ('http://63.35.219.5:3001');
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: {
             origin: true,
