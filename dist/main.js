c783710a23a24ec37b5bb607fa86f994ef68f744
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-02-10 12:35:06 +0100

    VUELTA NORMALIDAD

--- dist/main.js
+++ dist/main.js
@@ -4,11 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const axios_1 = require("axios");
 require('./sincro');
-<<<<<<< HEAD
-axios_1.default.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://63.35.219.5:3001') : ('http://63.35.219.5:3001');
-=======
 axios_1.default.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://localhost:3001') : ('http://34.78.247.153:3001');
->>>>>>> parent of 47ce931 (SERVIDOR)
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: {
             origin: true,
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 import axios from 'axios';
 require('./sincro');
 
-<<<<<<< HEAD
-axios.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://63.35.219.5:3001') : ('http://63.35.219.5:3001'); // NORMAL
-=======
 axios.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://localhost:3001') : ('http://34.78.247.153:3001'); // NORMAL
->>>>>>> parent of 47ce931 (SERVIDOR)
 // axios.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://localhost:3001') : ('http://localhost:3001'); // DEV LOCAL SANPEDRO
 
 async function bootstrap() {
