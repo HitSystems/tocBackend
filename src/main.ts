47ce9318edd2cdf5f27e3d9de0227d4200f89284
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-02-10 10:45:32 +0100

    SERVIDOR

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import axios from 'axios';
 require('./sincro');
 
-axios.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://localhost:3001') : ('http://34.78.247.153:3001'); // NORMAL
+axios.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://localhost:3001') : ('http://localhost:3001'); // NORMAL
 // axios.defaults.baseURL = (process.argv[2] === 'modoServer') ? ('http://localhost:3001') : ('http://localhost:3001'); // DEV LOCAL SANPEDRO
 
 async function bootstrap() {
