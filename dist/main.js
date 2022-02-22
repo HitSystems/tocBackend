a905893329e52e5a4f5647795f6fd5c0f372d4d3
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2022-02-22 08:29:38 +0100

    Informacion

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,7 @@ async function bootstrap() {
             origin: true,
             credentials: true
         } });
-    await app.listen(3030);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
